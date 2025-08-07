4bf9faa1e7449b745570d83ff9c0b425f074efcb
Kinedrift <97351913+rodriko333@users.noreply.github.com>
2025-08-07 21:38:40 +0300

    it really hurts!

--- js/save_ref.js
+++ js/save_ref.js
@@ -1,15 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const params = new URLSearchParams(window.location.search);
-  const ref = params.get("ref");
+const params = new URLSearchParams(window.location.search);
+const ref = params.get("ref");
 
-  if (!ref) return;
+if (!ref) return;
 
-  document.querySelectorAll("a[href]").forEach(link => {
-    const url = new URL(link.href, window.location.origin);
+document.querySelectorAll("a[href]").forEach(link => {
+  const url = new URL(link.href, window.location.origin);
 
-    if (url.origin === window.location.origin) {
-      url.searchParams.set("ref", ref);
-      link.href = url.toString();
-    }
-  });
+  if (url.origin === window.location.origin) {
+    url.searchParams.set("ref", ref);
+    link.href = url.toString();
+  }
 });
